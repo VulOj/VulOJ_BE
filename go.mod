081f680fe54bfbd1527f71185303d3f746957cf4
1720648723 <1720648723@qq.com>
2022-01-14 14:27:51 +0800

    add:port listener while gin

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module VulOJ_BE
 
 go 1.15
+
+require github.com/gin-gonic/gin v1.7.7
--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
+	r := gin.Default()
+	r.Run(":8080")
+}
+
+func main_test() {
 	fmt.Println("Hello World!\n")
 }
